a787ad5592aa35cf408b83fa16804264e7bcca11
Sektnan <robinsektnan@hotmail.com>
2024-10-23 21:34:44 +0200

    Importerte Z fra ZOD i "useProjects.ts".

--- frontend/src/hooks/useProjects.ts
+++ frontend/src/hooks/useProjects.ts
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import { fetchProjects } from '../services/api';
 import { ProjectSchema, ProjectType } from '../schemas/projectSchema';
+import { z } from 'zod';
+
 
 export const useProjects = () => {
   const [projects, setProjects] = useState<ProjectType[]>([]);
